8455ffb8b291206007600df78adefb6e478986b2
Maciek Zaweracz <mz1794@york.ac.uk>
2024-10-18 12:34:01 +0100

    Update positions_batch_multiprocess.py

--- Code/positions_batch_multiprocess.py
+++ Code/positions_batch_multiprocess.py
@@ -4,298 +4,192 @@
 Created on Thu May 21 20:17:55 2020
 
 @author: erick
+
+Modified on Fri October 18 11:59:55 2024
+
+@author: Maciek
 """
 
-# # Holographic Coordinate Detection
+# Holographic Coordinate Detection
 
-# %% Import libraries
+# Import libraries
 import os
-from multiprocessing import Pool
-from multiprocessing import cpu_count
+from multiprocessing import Pool, cpu_count
 from time import time
+from tqdm import tqdm
 
 import PySimpleGUI as sg
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
-
 import functions as f
 
-# %% GUI for selecting files and parameters
-if __name__ == '__main__':
+# Constant for opening File after export
+OPEN_FILE = True
 
-    # %
+# GUI for selecting files and parameters
+def main():
     layout = [
-        [sg.Text('Select AVi File recording',
-                 size=(35, 1)),
-         # sg.In(default_text='Select a video'),
+        [sg.Text('Select AVi File recording', size=(35, 1)),
          sg.FileBrowse(initial_folder='', key='-FILE-')],
-        [sg.Text('Select Background Image',
-                 size=(35, 1)),
-         # sg.In(default_text='Select a video'),
-         sg.FileBrowse(initial_folder='', key='-BG_IMAGE-')],
-        [sg.Text('Propagator Scheme', size=(35, 1)), sg.Radio('Rayleigh-Sommerfeld', 'SCHEME', key='-RS-'),
+        [sg.Text('Select Background Image', size=(35, 1)),
+         sg.FileBrowse(initial_folder='', key='-BGIMAGE-')],
+        [sg.Text('Propagator Scheme', size=(35, 1)),
+         sg.Radio('Rayleigh-Sommerfeld', 'SCHEME', key='-RS-', default=True),
          sg.Radio('Modified', 'SCHEME', key='-MOD-')],
         [sg.Text('Refraction index of media (water = 1.3226)', size=(35, 1)),
-         sg.InputText(default_text=1.3226, key='-N-')],
-        [sg.Text('Wavelength in um (~0.642)', size=(35, 1)), sg.InputText(default_text=0.642, key='-WAVELENGTH-')],
-        [sg.Text('Magnification (10, 20, etc)', size=(35, 1)), sg.InputText(default_text=10, key='-MPP-')],
-        [sg.Text('Step size (10)', size=(35, 1)), sg.InputText(default_text=10, key='-SZ-')],
-        [sg.Text('Number os steps (150)', size=(35, 1)), sg.InputText(default_text=150, key='-NUMSTEPS-')],
+         sg.InputText(default_text=1.3326, key='-N-')],
+        [sg.Text('Wavelength in um (~0.642)', size=(35, 1)),
+         sg.InputText(default_text=0.642, key='-WAVELENGTH-')],
+        [sg.Text('Magnification (10, 20, etc)', size=(35, 1)),
+         sg.InputText(default_text=40, key='-MPP-')],
+        [sg.Text('Step size (10)', size=(35, 1)),
+         sg.InputText(default_text=3, key='-SZ-')],
+        [sg.Text('Number of steps (150)', size=(35, 1)),
+         sg.InputText(default_text=30, key='-NUMSTEPS-')],
         [sg.Text('Gradient Stack Threshold (~0.1) just for RS', size=(35, 1)),
-         sg.InputText(default_text=0.1, key='-THRESHOLD-')],
-        [sg.Text('Peak Min Distance (20, 40, 60)', size=(35, 1)), sg.InputText(default_text=20, key='-PMD-')],
-        [sg.Text('Frame Rate', size=(35, 1)), sg.InputText(default_text=50, key='-FRAMERATE-')],
+         sg.InputText(default_text=0.002, key='-THRESHOLD-')],
+        [sg.Text('Peak Min Distance (20, 40, 60)', size=(35, 1)),
+         sg.InputText(default_text=20, key='-PMD-')],
+        [sg.Text('Frame Rate', size=(35, 1)),
+         sg.InputText(default_text=50, key='-FRAMERATE-')],
         [sg.Checkbox('Invert Video', default=False, key='-INVERT-')],
         [sg.Checkbox('Export as CSV', default=True, key='-EXPORT-')],
-        [sg.Text('Number of frames for calculations', size=(35, 1)), sg.InputText(default_text='', key='-NUMFRAMES-')],
+        [sg.Text('Number of frames for calculations', size=(35, 1)),
+         sg.InputText(default_text='50', key='-NUMFRAMES-')],
         [sg.Button('Add File'), sg.Button('Start'), sg.Cancel()]
     ]
 
     window = sg.Window('Holography video inputs', layout)
 
-    PATH = []
-    BG_PATH = []
-    N = []
-    LAMBDA = []
-    MPP = []
-    SZ = []
-    NUMSTEPS = []
-    THRESHOLD = []
-    PMD = []
-    FRAME_RATE = []
-    INVERT_VIDEO = []
-    export = []
-    num_frames = []
-    scheme = []
-    params = []
+    # Initialize data storage
+    paths, bg_paths, params = [], [], []
 
     while True:
-        event, values = window.Read()
-        # values = list(values.values())
-        if event == 'Cancel':  # or event == sg.WIN_CLOSED:
+        event, values = window.read()
+
+        if event in (sg.WIN_CLOSED, 'Cancel'):
             break
         elif event == 'Start':
             break
         elif event == 'Add File':
-            # print('File '+os.path.split(values[0])[-1]+' added to queue')
-            # PATH.append(values[0])
-            # N.append(float(values[1]))
-            # LAMBDA.append(float(values[2]))
-            # MPP.append(int(values[3]))
-            # SZ.append(float(values[4]))
-            # NUMSTEPS.append(int(values[5]))
-            # THRESHOLD.append(float(values[6]))
-            # PMD.append(int(values[7]))
-            # FRAME_RATE.append(int(values[8]))
-            # INVERT_VIDEO.append(values[9])          
-            # export.append(values[10])
+            print(f'File {os.path.split(values["-FILE-"])[-1]} added to queue')
+            #paths.append(values['-FILE-'])
+            #bg_paths.append(values['-BGIMAGE-'])
+            paths.append(
+                'C:/Users/mz1794/Downloads/Python and Viking DHM port-20241010T094616Z-001/Python and Viking DHM port/40x_100Hz_1081_CHO_1_T5_detrend_frame0-50.avi')
+            bg_paths.append(
+                'C:/Users/mz1794/Downloads/Python and Viking DHM port-20241010T094616Z-001/Python and Viking DHM port/1024bg.png')
 
-            # if values[11] == '':
-            #     num_frames.append([])
-            # else:
-            #     num_frames.append(int(values[11]))
-            # params.append(pd.DataFrame([values[1:]], columns=['N', 'Wavelength', 'MPP', 'SZ', 'Step #', 'Threshold', 'MPD', 'FR','Invert', 'Export', 'Num Frames']))
+            scheme = '-RS-' if values['-RS-'] else '-MOD-'
+            params.append({
+                'N': float(values['-N-']),
+                'Wavelength': float(values['-WAVELENGTH-']),
+                'MPP': int(values['-MPP-']),
+                'SZ': float(values['-SZ-']),
+                'NUMSTEPS': int(values['-NUMSTEPS-']),
+                'THRESHOLD': float(values['-THRESHOLD-']),
+                'PMD': int(values['-PMD-']),
+                'FRAMERATE': int(values['-FRAMERATE-']),
+                'INVERT': values['-INVERT-'],
+                'EXPORT': values['-EXPORT-'],
+                'NUMFRAMES': int(values['-NUMFRAMES-']) if values['-NUMFRAMES-'] else None,
+                'SCHEME': scheme
+            })
 
-            ##########################################################
-            print('File ' + os.path.split(values['-FILE-'])[-1] + ' added to queue')
-            PATH.append(values['-FILE-'])
-            BG_PATH.append(values['BG_IMAGE'])
+    window.close()
+    print('-' * 150)
 
-            if values['-RS-'] == True:
-                scheme.append('-RS-')
-            else:
-                scheme.append('-MOD-')
-
-            N.append(float(values['-N-']))
-            LAMBDA.append(float(values['-WAVELENGTH-']))
-            MPP.append(int(values['-MPP-']))
-            SZ.append(float(values['-SZ-']))
-            NUMSTEPS.append(int(values['-NUMSTEPS-']))
-            THRESHOLD.append(float(values['-THRESHOLD-']))
-            PMD.append(int(values['-PMD-']))
-            FRAME_RATE.append(int(values['-FRAMERATE-']))
-            INVERT_VIDEO.append(values['-INVERT-'])
-            export.append(values['-EXPORT-'])
-
-            if values['-NUMFRAMES-'] == '':
-                num_frames.append([])
-            else:
-                num_frames.append(int(values['-NUMFRAMES-']))
-            params.append(pd.DataFrame([list(values.values())[1:]],
-                                       # columns=['N', 'Wavelength', 'MPP', 'SZ', 'Step #', 'Threshold', 'MPD', 'FR','Invert', 'Export', 'Num Frames']
-                                       columns=list(values.keys())[1:]
-                                       )
-                          )
-
-            ##########################################################
-
-    window.Close()
-    print(
-        '---------------------------------------------------------------------------------------------------------------------------------------------------')
-
-    # %% Position detection
-
+    # Position detection
     data = []
     times = []
-    for k in range(len(PATH)):
-        VID = f.videoImport(PATH[k], 0)
-        BG_IMAGE = f.bgPathToArray(BG_IMAGE)
-        ni, nj, nk = np.shape(VID)
 
-        if INVERT_VIDEO[k]:
+    for k, path in enumerate(paths):
+        vid = f.videoImport(path, 0)
+        bg_image = f.bgPathToArray(bg_paths[k])
+        ni, nj, nk = vid.shape
+
+        if params[k]['INVERT']:
             for i in range(nk):
-                VID[:, :, i] = VID[:, :, i].max() - VID[:, :, i]
+                vid[:, :, i] = vid[:, :, i].max() - vid[:, :, i]
 
-        FRAMES_MEDIAN = 20
-        I_MEDIAN = f.medianImage(VID, FRAMES_MEDIAN)
-        # I_MEDIAN = np.ones((VID.shape[0], VID.shape[1]))
+        frames_median = 20
+        i_median = f.medianImage(vid, frames_median)
 
-        if num_frames[k] == []:
-            NUM_FRAMES = nk
-        else:
-            NUM_FRAMES = num_frames[k]
+        num_frames = params[k]['NUMFRAMES'] if params[k]['NUMFRAMES'] is not None else nk
 
-        IT = np.empty((NUM_FRAMES), dtype=object)
-        MED = np.empty((NUM_FRAMES), dtype=object)
-        n = np.empty((NUM_FRAMES), dtype=object)
-        lam = np.empty((NUM_FRAMES), dtype=object)
-        mpp = np.empty((NUM_FRAMES), dtype=object)
-        sz = np.empty((NUM_FRAMES), dtype=object)
-        numsteps = np.empty((NUM_FRAMES), dtype=object)
-        threshold = np.empty((NUM_FRAMES), dtype=object)
-        pmd = np.empty((NUM_FRAMES), dtype=object)
+        it = np.empty((num_frames), dtype=object)
+        med = np.empty((num_frames), dtype=object)
 
-        for i in range(NUM_FRAMES):
-            IT[i] = VID[:, :, i]
-            MED[i] = I_MEDIAN
-            n[i] = N[k]
-            lam[i] = LAMBDA[k]
-            mpp[i] = MPP[k]
-            sz[i] = SZ[k]
-            numsteps[i] = NUMSTEPS[k]
-            threshold[i] = THRESHOLD[k]
-            pmd[i] = PMD[k]
+        for i in range(num_frames):
+            it[i] = vid[:, :, i]
+            med[i] = i_median
+
+        # Parameters setup
+        n, lam, mpp, sz, numsteps = (params[k]['N'], params[k]['Wavelength'],
+                                     params[k]['MPP'], params[k]['SZ'],
+                                     params[k]['NUMSTEPS'])
+        threshold, pmd = (params[k]['THRESHOLD'], params[k]['PMD'])
 
         pool = Pool(cpu_count())  # Number of cores to use
         results = []
-        T0 = time()
+        t0 = time()
 
-        print('Processing File ' + str(k + 1) + ' of ' + str(len(PATH)) + ': ' + os.path.split(PATH[k])[-1])
-        print('Parameters: ')
-        print(params[k])
+        print(f'Processing File {k + 1} of {len(paths)}: {os.path.split(path)[-1]}')
+        print('Parameters:', params[k])
 
-        if scheme[k] == '-RS-':
-            for _ in tqdm(pool.imap_unordered(f.positions_batch,
-                                              zip(IT, MED, n, lam, mpp, sz, numsteps, threshold, pmd, BG_IMAGE)),
-                          total=NUM_FRAMES):
-                results.append(_)
+        scheme_func = f.positions_batch if params[k]['SCHEME'] == '-RS-' else f.positions_batch_modified
 
-        elif scheme[k] == '-MOD-':
-            for _ in tqdm(pool.imap_unordered(f.positions_batch_modified,
-                                              zip(IT, MED, n, lam, mpp, sz, numsteps, threshold, pmd)),
-                          total=NUM_FRAMES):
-                results.append(_)
+        for _ in tqdm(pool.imap_unordered(scheme_func,
+                                          zip(it, med, [n] * num_frames,
+                                              [lam] * num_frames, [mpp] * num_frames,
+                                              [sz] * num_frames, [numsteps] * num_frames,
+                                              [threshold] * num_frames, [pmd] * num_frames,
+                                              bg_image)), total=num_frames):
+            results.append(_)
 
-        times.append(time() - T0)
-
-        # for i, item in enumerate(pool.imap_unordered(f.positions_batch, zip(IT, MED, n, lam, mpp, sz, numsteps, threshold, pmd))):
-        #     sg.one_line_progress_meter('This is my progress meter!', 
-        #                                i+1, 
-        #                                NUM_FRAMES,
-        #                                'Processing File '+str(k+1)+' of '+str(len(PATH))+': ',
-        #                                os.path.split(PATH[k])[-1],
-        #                                values[1:],
-        #                                orientation='h')
-        #     results.append(item)
-
+        times.append(time() - t0)
         pool.close()
         pool.join()
 
-        POSITIONS = pd.DataFrame(columns=['X', 'Y', 'Z', 'I_FS', 'I_GS', 'FRAME', 'TIME'])
-        for i in range(NUM_FRAMES):
-            X = results[i][0][0]
-            Y = results[i][1][0]
-            Z = results[i][2][0]
-            I_FS = results[i][3][0]
-            I_GS = results[i][4][0]
-            FRAME = i * np.ones_like(results[i][0][0])
-            TIME = FRAME / FRAME_RATE[k]
+        positions = pd.DataFrame(columns=['X', 'Y', 'Z', 'I_FS', 'I_GS', 'FRAME', 'TIME'])
 
-            DATA = np.concatenate((np.expand_dims(X, axis=1),
-                                   np.expand_dims(Y, axis=1),
-                                   np.expand_dims(Z, axis=1),
-                                   np.expand_dims(I_FS, axis=1),
-                                   np.expand_dims(I_GS, axis=1),
-                                   np.expand_dims(FRAME, axis=1),
-                                   np.expand_dims(TIME, axis=1)),
-                                  axis=1)
-            # POSITIONS = POSITIONS.append(pd.DataFrame(DATA, 
-            # columns=['X', 'Y', 'Z', 'I_FS', 'I_GS', 'FRAME', 'TIME']))
+        for i in range(num_frames):
 
-            POSITIONS = pd.concat([POSITIONS, pd.DataFrame(DATA,
+            x = results[i][0][0]
+            y = results[i][1][0]
+            z = results[i][2][0]
+            i_fs = results[i][3][0]
+            i_gs = results[i][4][0]
+
+            frame = np.full_like(x, i)
+            time_values = frame / params[k]['FRAMERATE']
+
+            data_row = np.column_stack((x, y, z, i_fs, i_gs, frame, time_values))
+            positions = pd.concat([positions, pd.DataFrame(data_row,
                                                            columns=['X', 'Y', 'Z', 'I_FS', 'I_GS', 'FRAME', 'TIME'])],
                                   ignore_index=True)
 
-        POSITIONS = POSITIONS.astype('float')
-        POSITIONS['TIME'] = POSITIONS['TIME'].round(3)
+        positions = positions.astype('float')
+        positions['TIME'] = positions['TIME'].round(3)
 
-        # %% Export CSV Files
+        # Export CSV Files
+        if params[k]['EXPORT']:
+            path = os.path.split(path)[:-1][0]
+            pp = os.path.split(path)[-1][:-4]
+            scheme_suffix = 'RS' if params[k]['SCHEME'] == '-RS-' else 'MOD'
+            expath = f"{path}/{pp}_TH{params[k]['THRESHOLD']}_PMD{params[k]['PMD']}_SZ{params[k]['SZ']}_NUMSTEPS{params[k]['NUMSTEPS']}_{scheme_suffix}.csv"
+            positions.to_csv(expath)
 
-        if export[k]:
-            path = os.path.split(PATH[k])[:-1][0]
-            pp = os.path.split(PATH[k])[-1][:-4]
+            print('Exported to:\n', expath)
+            print('-' * 150)
 
-            if scheme[k] == '-RS-':
-                expath = path + '/' + pp + '_TH' + str(THRESHOLD[k]) + '_PMD' + str(PMD[k]) + '_SZ' + str(
-                    SZ[k]) + '_NUMSTEPS' + str(NUMSTEPS[k]) + '_RS' + '.csv'
-                POSITIONS.to_csv(expath)
+            if OPEN_FILE:
+                os.startfile(expath)
 
-            elif scheme[k] == '-MOD-':
-                expath = path + '/' + pp + '_TH' + str(THRESHOLD[k]) + '_PMD' + str(PMD[k]) + '_SZ' + str(
-                    SZ[k]) + '_NUMSTEPS' + str(NUMSTEPS[k]) + '_MOD' + '.csv'
-                POSITIONS.to_csv(expath)
-            print('Exported to: \n', expath)
-            print(
-                '---------------------------------------------------------------------------------------------------------------------------------------------------')
-
-        #     T = time.time - T0
-        #     times.append(T)
-        data.append(POSITIONS)
+        data.append(positions)
 
     print('Done!')
 
-    plot = False
-    if plot:
-        # %% Scatter Plot
-        import pandas as pd
-        from matplotlib import pyplot
 
-        # # %matplotlib qt
-        # # %matplotlib inline
-
-        # path = gui.fileopenbox(default='/media/erick/NuevoVol/LINUX_LAP/PhD/Thesis/', multiple=False)
-        # POSITIONS = pd.read_csv(path)
-        POSITIONS = data[0]
-        # # POSITIONS = DF
-
-        X = POSITIONS.X
-        Y = POSITIONS.Y
-        Z = POSITIONS.Z
-        T = POSITIONS.FRAME
-
-        # # maxx = Z == Z.max()
-
-        fig = pyplot.figure(1, figsize=(5, 5))
-        ax = pyplot.axes(projection='3d')
-
-        ax.scatter(X, Y, Z, s=1, marker='.', c=T)
-        # #ax.plot(X, Y, Z)
-        # # ax.scatter(X[maxx], Y[maxx], Z[maxx], c='red')
-        ax.tick_params(axis='both', labelsize=10)
-        ax.set_title('Cells Positions in 3D', fontsize='20')
-        ax.set_xlabel('y (um)', fontsize='18')
-        ax.set_ylabel('x (um)', fontsize='18')
-        ax.set_zlabel('z (um)', fontsize='18')
-
-        pyplot.show()
+if __name__ == '__main__':
+    main()
