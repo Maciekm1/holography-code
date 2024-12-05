2363aaba95b0e25ebd334aea9bf604648bf77cbd
M Zaweracz <mz1794@login2.viking2.yor.alces.network>
2024-12-05 10:18:54 +0000

    Load Modules on one line in sh scripts

--- Code/start.sh
+++ Code/start.sh
@@ -5,12 +5,12 @@ set -e
 
 # Purge existing modules and load required ones
 module purge
-module load Python/3.10.4-GCCcore-11.3.0
-module load tqdm/4.64.0-GCCcore-11.3.0
-module load SciPy-bundle/2022.05-foss-2022a
-module load scikit-image/0.19.3-foss-2022a
-module load OpenCV/4.6.0-foss-2022a-contrib
-module load openpyxl/3.0.10-GCCcore-11.3.0
+ml load Python/3.10.4-GCCcore-11.3.0 tqdm/4.64.0-GCCcore-11.3.0 SciPy-bundle/2022.05-foss-2022a scikit-image/0.19.3-foss-2022a OpenCV/4.6.0-foss-2022a-contrib openpyxl/3.0.10-GCCcore-11.3.0
+#module load tqdm/4.64.0-GCCcore-11.3.0
+#module load SciPy-bundle/2022.05-foss-2022a
+#module load scikit-image/0.19.3-foss-2022a
+#module load OpenCV/4.6.0-foss-2022a-contrib
+#module load openpyxl/3.0.10-GCCcore-11.3.0
 
 echo "Modules loaded successfully..."
 echo ""
@@ -24,10 +24,8 @@ echo "Python script returned: $output_folder_frames"
 # Use the captured output as a parameter for the next command
 if [[ -d "$output_folder_frames" ]]; then
     echo "Directory exists: $output_folder_frames"
-    
     # Run the update_job_array.sh script with the output directory path
     ./update_job_array.sh "$output_folder_frames"
-    
     # Ask the user if they want to monitor the job queue
     read -p "Do you want to monitor the job queue with 'watch'? (y/n): " user_input
     if [[ "$user_input" == "y" || "$user_input" == "Y" ]]; then
